8912f80c81d4a6bf22c69df499b7f2ce9ee39c47
Sofia Damaso <57020694+sofidamaso@users.noreply.github.com>
2023-02-08 23:51:14 +0100

    position headers

--- src/cover.js
+++ src/cover.js
@@ -2,13 +2,21 @@ $.fn.setPaddingTop = function() {
     let paddingTop = $('.image-cover').height();
     let paperHeight = $('.paper-cover').height();
     paddingTop = paddingTop - paperHeight/2 ;
-    return this.css("padding-top", paddingTop);
+    return this.css("margin-top", paddingTop);
+};
+$.fn.setPaddingTopText = function() {
+    let paddingTopText = $('.image-cover').height();
+    let paperHeight = $('.paper-cover').height();
+    paddingTopText = (paddingTopText - paperHeight) - paddingTopText*0.10;
+    return this.css("margin-top", paddingTopText);
 };
 
 $(document).ready(function() {
     $('.paper-cover').setPaddingTop();
+    $('.panel h1').setPaddingTopText();
 });
 
 $(window).resize(function(){
     $('.paper-cover').setPaddingTop();
+    $('.panel h1').setPaddingTopText();
 });
