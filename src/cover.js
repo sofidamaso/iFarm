5e690fc6ffc0a001387ec038e0d948be5e7bcb21
valef99 <vale.f99music@gmail.com>
2023-02-09 13:07:25 +0100

    added images and mobile/desktop animation managing

--- src/cover.js
+++ src/cover.js
@@ -1,13 +1,31 @@
+$.fn.countLinesText = function(){
+    let divHeight = $('.panel h1').outerHeight();
+    let lineHeight = parseInt($('.panel h1').css("line-height").replace("px",""));
+    return Math.floor(divHeight / lineHeight);
+}
+
 $.fn.setPaddingTop = function() {
-    let paddingTop = $('.image-cover').height();
+    let paddingTop = 0;
+    if($('.image-cover:eq(0)').is(':hidden'))
+        paddingTop = $('.image-cover:eq(1)').height();
+    else
+        paddingTop = $('.image-cover:eq(0)').height();
     let paperHeight = $('.paper-cover').height();
     paddingTop = paddingTop - paperHeight/2 ;
     return this.css("margin-top", paddingTop);
 };
 $.fn.setPaddingTopText = function() {
-    let paddingTopText = $('.image-cover').height();
+    let paddingTopText = 0;
+    if($('.image-cover:eq(0)').is(':hidden'))
+        paddingTopText = $('.image-cover:eq(1)').height();
+    else
+        paddingTopText = $('.image-cover:eq(0)').height();
     let paperHeight = $('.paper-cover').height();
-    paddingTopText = (paddingTopText - paperHeight) - paddingTopText*0.10;
+    let lines = $('.panel h1').countLinesText();
+    if(lines>1)
+        paddingTopText = (paddingTopText - paperHeight) - $('.panel h1').outerHeight();
+    else
+        paddingTopText = (paddingTopText - paperHeight) - paddingTopText*0.10;
     return this.css("margin-top", paddingTopText);
 };
 
